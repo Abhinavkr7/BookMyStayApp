ddf62e740496737906dc1914f7c656c80e5af400
Abhinavkr7 <imabhinav.21@gmail.com>
2026-03-18 15:42:47 +0530

    Remove unnecessary code block formatting

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,4 +1,4 @@
-```java
+
 public class RoomSearchService {
 
     /**
@@ -40,4 +40,4 @@ public class RoomSearchService {
         System.out.println("\n============================");
     }
 }
-```
+
